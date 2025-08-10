dd7401fa962003d3e490dc1ef297d419bb21712f
annieha <hajh5447@gmail.com>
2025-08-10 16:31:15 +0900

    [bugfix] comment userreport filter

--- src/main/java/com/may21/trobl/report/ReportService.java
+++ src/main/java/com/may21/trobl/report/ReportService.java
@@ -18,7 +18,8 @@ public class ReportService {
 
     public int report(Long userId, Long targetId, ItemType targetType,
             ReportDto.Request reportRequest) {
-        if (!reportRepository.existsByTargetIdAndTargetTypeAndReportedBy(targetId, targetType, userId)) {
+        if (!reportRepository.existsByTargetIdAndTargetTypeAndReportedBy(targetId, targetType,
+                userId)) {
 
             Report report = new Report(targetId, targetType, userId, reportRequest);
             reportRepository.save(report);
@@ -27,7 +28,8 @@ public class ReportService {
     }
 
     public List<Long> getBlockedTargetIds(Long userId, List<Long> targetIds, ItemType targetType) {
-        return reportRepository.findBlockedIdsByUserIdAndTargetTypeInTargetIds(userId, targetType, targetIds);
+        return reportRepository.findBlockedIdsByUserIdAndTargetTypeInTargetIds(userId, targetType,
+                targetIds);
     }
 
     public List<Long> getBlockedTargetIds(Long userId, ItemType targetType) {
@@ -38,7 +40,9 @@ public class ReportService {
         List<Long> commentIds = comments.stream()
                 .map(Comment::getId)
                 .toList();
-        List<Report> reports = reportRepository.getRelatedReports(userId, commentIds, ItemType.COMMENT, ItemType.USER);
+        List<Report> reports =
+                reportRepository.getRelatedReports(userId, commentIds, ItemType.COMMENT,
+                        ItemType.USER);
         List<Long> blockedCommentIds = reports.stream()
                 .filter(report -> report.getTargetType() == ItemType.COMMENT)
                 .map(Report::getTargetId)
@@ -47,12 +51,12 @@ public class ReportService {
                 .filter(report -> report.getTargetType() == ItemType.USER)
                 .map(Report::getTargetId)
                 .toList();
-        if (!blockedCommentIds.isEmpty()) {
-            comments = comments.stream()
-                    .filter(comment -> !blockedCommentIds.contains(comment.getId())
-                            && !blockedUserIds.contains(comment.getUserId()))
-                    .toList();
-        }
+        if (blockedCommentIds.isEmpty() && blockedUserIds.isEmpty()) return comments;
+        comments = comments.stream()
+                .filter(comment -> !blockedCommentIds.contains(comment.getId()) &&
+                        !blockedUserIds.contains(comment.getUserId()))
+                .toList();
+
         return comments;
     }
 
@@ -60,7 +64,8 @@ public class ReportService {
         List<Long> postIds = posts.stream()
                 .map(Posting::getId)
                 .toList();
-        List<Report> reports = reportRepository.getRelatedReports(userId, postIds, ItemType.POST, ItemType.USER);
+        List<Report> reports =
+                reportRepository.getRelatedReports(userId, postIds, ItemType.POST, ItemType.USER);
         List<Long> blockedPostIds = reports.stream()
                 .filter(report -> report.getTargetType() == ItemType.POST)
                 .map(Report::getTargetId)
@@ -71,8 +76,8 @@ public class ReportService {
                 .toList();
         if (!blockedPostIds.isEmpty()) {
             posts = posts.stream()
-                    .filter(post -> !blockedPostIds.contains(post.getId())
-                            && !blockedUserIds.contains(post.getUserId()))
+                    .filter(post -> !blockedPostIds.contains(post.getId()) &&
+                            !blockedUserIds.contains(post.getUserId()))
                     .toList();
         }
         return posts;
